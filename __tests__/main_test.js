252632ed8d8194100579169f97780bc5a7b5d9b9
AnthonyWhiteDev <a.beuchey@whitedev.fr>
2022-11-17 11:47:30 +0100

    Feat (node.js.yml): To run eslint

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -4,7 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(main.sum(1, 2)).toBe(3);
 });
 
-/*
 test('adds 1 + 2 to equal 3', () => {
     expect(main.subtract(1, 2)).toBe(-1);
-});*/
+});
--- src/main.js
+++ src/main.js
@@ -6,4 +6,7 @@ function subtract(a, b) {
     return a - b;
 }
 
-module.exports = { sum, subtract };
+module.exports = {
+    sum: sum,
+    subtract: subtract
+};
