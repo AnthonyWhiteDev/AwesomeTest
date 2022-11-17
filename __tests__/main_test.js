c54386276eb524f8124be08e394d73abd674aa7a
AnthonyWhiteDev <a.beuchey@whitedev.fr>
2022-11-17 11:30:57 +0100

    Feat (_): Setup tests with coverage

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -1,5 +1,9 @@
-const sum = require('../src/main');
+const main = require('../src/main');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(main.sum(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(main.subtract(1, 2)).toBe(-1);
 });
