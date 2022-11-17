2d5858a6e4212aa66d0b720eac14258bec82c944
AnthonyWhiteDev <a.beuchey@whitedev.fr>
2022-11-17 11:36:19 +0100

    Test (main_test.js): CI fails if test coverage under 100

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -4,6 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
     expect(main.sum(1, 2)).toBe(3);
 });
 
+/*
 test('adds 1 + 2 to equal 3', () => {
-    expect(main.subtract(1, 2)).toBe(3);
-});
+    expect(main.subtract(1, 2)).toBe(-1);
+});*/
